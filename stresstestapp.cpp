2a9c5039f1f289a0958dafe04e78e5328a3ffb2c
kirathenics <bka32129@gmail.com>
2024-06-26 12:13:09 +0300

    added stresstester classes stop function

--- stresstestapp.cpp
+++ stresstestapp.cpp
@@ -2,50 +2,11 @@
 #include "ui_stresstestapp.h"
 
 
-#include <QThread>
-#include <QDebug>
-#include <QtGlobal>
-#include <QtMath>
-#include <QRandomGenerator>
-
-class CPUStressTester : public QThread {
-public:
-    void run() override {
-        qDebug() << "Starting CPU stress test";
-        double result = 0;
-        while (!isInterruptionRequested()) {
-            for (int i = 0; i < 1000000; ++i) {
-//                result += qSin(qrand());
-                result += qSin(QRandomGenerator::global()->generate());
-            }
-            QThread::msleep(10); // Имитация нагрузки
-        }
-        qDebug() << "CPU stress test stopped";
-    }
-};
-
-class FPUStressTester : public QThread {
-public:
-    void run() override {
-        qDebug() << "Starting FPU stress test";
-        double result = 0;
-        while (!isInterruptionRequested()) {
-            for (int i = 0; i < 1000000; ++i) {
-//                result += qSqrt(qrand());
-                result += qSqrt(QRandomGenerator::global()->generate());
-            }
-            QThread::msleep(10); // Имитация нагрузки
-        }
-        qDebug() << "FPU stress test stopped";
-    }
-};
-
-
-
-
 StressTestApp::StressTestApp(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::StressTestApp)
+    , cpuStressTester(nullptr)
+    , fpuStressTester(nullptr)
 {
     ui->setupUi(this);
 
@@ -55,13 +16,25 @@ StressTestApp::StressTestApp(QWidget *parent)
     timer->setInterval(1000);
     time = new QTime(0, 0, 0);
 
-    //ui->cpu_checkBox->setChecked(true);
+    ui->cpu_checkBox->setChecked(true);
+    ui->fpu_checkBox->setChecked(true);
 
     connect(timer, &QTimer::timeout, this, &StressTestApp::updateTimerLabel);
 }
 
 StressTestApp::~StressTestApp()
 {
+    if (cpuStressTester) {
+        cpuStressTester->requestInterruption();
+        cpuStressTester->wait();
+        delete cpuStressTester;
+    }
+    if (fpuStressTester) {
+        fpuStressTester->requestInterruption();
+        fpuStressTester->wait();
+        delete fpuStressTester;
+    }
+
     delete ui;
 }
 
@@ -112,14 +85,23 @@ void StressTestApp::on_start_pushButton_clicked()
     startOrResumeTimer();
 
     if (ui->cpu_checkBox->isChecked()) {
-        CPUStressTester* cpuStressTester = new CPUStressTester();
+        if (cpuStressTester) {
+            cpuStressTester->requestInterruption();
+            cpuStressTester->wait();
+            delete cpuStressTester;
+        }
+        cpuStressTester = new CPUStressTester();
         cpuStressTester->start();
     }
     if (ui->fpu_checkBox->isChecked()) {
-        FPUStressTester* fpuStressTester = new FPUStressTester();
+        if (fpuStressTester) {
+            fpuStressTester->requestInterruption();
+            fpuStressTester->wait();
+            delete fpuStressTester;
+        }
+        fpuStressTester = new FPUStressTester();
         fpuStressTester->start();
     }
-    //cpuStress();
 }
 
 
@@ -128,6 +110,19 @@ void StressTestApp::on_stop_pushButton_clicked()
     if (!ui->start_pushButton->isEnabled()) {
         ui->start_pushButton->setEnabled(true);
         stopTimer();
+
+        if (cpuStressTester) {
+            cpuStressTester->requestInterruption();
+            cpuStressTester->wait();
+            delete cpuStressTester;
+            cpuStressTester = nullptr;
+        }
+        if (fpuStressTester) {
+            fpuStressTester->requestInterruption();
+            fpuStressTester->wait();
+            delete fpuStressTester;
+            fpuStressTester = nullptr;
+        }
     }
 }
 
--- stresstestapp.h
+++ stresstestapp.h
@@ -6,9 +6,46 @@
 #include <QTimer>
 #include <QTime>
 
+#include <QThread>
+#include <QDebug>
+#include <QtMath>
+#include <QRandomGenerator>
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class StressTestApp; }
 QT_END_NAMESPACE
+
+
+class CPUStressTester : public QThread {
+public:
+    void run() override {
+        qDebug() << "Starting CPU stress test";
+        double result = 0;
+        while (!isInterruptionRequested()) {
+            for (int i = 0; i < 1000000; ++i) {
+                result += qSin(QRandomGenerator::global()->generate());
+            }
+            QThread::msleep(10); // Имитация нагрузки
+        }
+        qDebug() << "CPU stress test stopped";
+    }
+};
+
+class FPUStressTester : public QThread {
+public:
+    void run() override {
+        qDebug() << "Starting FPU stress test";
+        double result = 0;
+        while (!isInterruptionRequested()) {
+            for (int i = 0; i < 1000000; ++i) {
+                result += qSqrt(QRandomGenerator::global()->generate());
+            }
+            QThread::msleep(10); // Имитация нагрузки
+        }
+        qDebug() << "FPU stress test stopped";
+    }
+};
+
 
 class StressTestApp : public QMainWindow
 {
@@ -26,6 +63,9 @@ private:
     Ui::StressTestApp *ui;
     QTimer* timer;
     QTime* time;
+
+    CPUStressTester* cpuStressTester;
+    FPUStressTester* fpuStressTester;
 
     void updateCurrentDateTime();
     void startOrResumeTimer();
